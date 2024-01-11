61a042bf710ccabb340466c604311d79c8ae3dbf
Guilherme Oliveira dos Santos <141884720+GuilhermeSantosz@users.noreply.github.com>
2024-01-11 09:50:29 -0300

    Update main.js

--- main.js
+++ main.js
@@ -8,31 +8,38 @@ function Carro(marca, rival) {
 }
 
     
-    function Modelo(marca, modelo, ano, rival) {
+    function Modelo(marca, modelo, ano, rival, motor) {
         this.modelo = modelo
+        this.motor = motor
         // this.ano = ano
         let _ano = ano
 
         this.getAno = function() {
             return `O melhor ano do ${modelo} é ${_ano}`
         }
-    
         this.dizModelo = function() {
             console.log("Melhor modelo: " + this.modelo)
         } 
         this.dizAno = function() {
             console.log(this.getAno())
-        }    
+        } 
+        this.dizMotor = function() {
+            console.log("O motor era um: " + this.motor)
+        }   
         
     Carro.call(this, marca, rival)
     
 }
 
-const carro = new Modelo("Ford", "Camaro", 1986, "Ferrari")
+const carro = new Modelo("Ford", "Camaro", 1986, "Ferrari", "V8")
 
 carro.dizOi()
 carro.dizModelo()
+carro.dizMotor()
 
 console.log(carro.getAno())
+
+
+
 
 
